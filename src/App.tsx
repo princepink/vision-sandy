be7ed11422e51306af825ba8522878370c1231d0
princepink <git@pearlpuppy.com>
2026-02-05 16:51:01 +0900

    feat: page transfer with Motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import '@mantine/core/styles.css'; // スタイルのインポートも忘れずに！
 import { MantineProvider } from '@mantine/core';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { useLocation, /*BrowserRouter,*/ Routes, Route, Link } from 'react-router-dom';
 import { AppShell, Burger, NavLink, Stack } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+
+// import { useLocation, Routes, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { PageWrapper } from './components/Motion/PageWrapper';
 
 // 各ページコンポーネント（後ほど作成）
 import Home from './pages/Home';
@@ -13,10 +17,11 @@ import GsapLenisPage from './pages/GsapLenis';
 
 export default function App() {
   const [opened, { toggle }] = useDisclosure();
+  const location = useLocation(); // 現在のパスを取得
 
   return (
     <MantineProvider>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
 
         <AppShell
           header={{ height: 60 }}
@@ -36,20 +41,21 @@ export default function App() {
               <NavLink component={Link} to="/gsap-lenis" label="GSAP + Lenis" />
             </Stack>
           </AppShell.Navbar>
-  
-          <AppShell.Main>
-
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/react-bits" element={<ReactBitsPage />} />
-              <Route path="/motion" element={<MotionPage />} />
-              <Route path="/gsap-lenis" element={<GsapLenisPage />} />
+        {/* AppShellなどのレイアウト */}
+        <AppShell.Main>
+          {/* mode="wait" は「前のページが消えてから次を出す」設定 */}
+          <AnimatePresence mode="wait">
+            {/* locationを渡すことで、パスが変わるたびにアニメーションが発動する */}
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+              <Route path="/react-bits" element={<PageWrapper><ReactBitsPage /></PageWrapper>} />
+              <Route path="/motion" element={<PageWrapper><MotionPage /></PageWrapper>} />
+              <Route path="/gsap-lenis" element={<PageWrapper><GsapLenisPage /></PageWrapper>} />
             </Routes>
-
-          </AppShell.Main>
+          </AnimatePresence>
+        </AppShell.Main>
         </AppShell>
-
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </MantineProvider>
   );
 }
